4450864f4fbcfe4365c1bcb364d0e31af8c865e0
BurakAy <burak@burak-aydemir.com>
2018-11-29 19:44:27 -0600

    setting up

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const layout = (props) => (
+    <>
+        <div>
+
+        </div>
+        <main>
+            {props.children}
+        </main>
+    </>
+);
+
+export default layout;
