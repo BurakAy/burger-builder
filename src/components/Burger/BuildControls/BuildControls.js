5e1a3da836ae0e1f06e12b165b7e272f90fcf5fb
BurakAy <burak@burak-aydemir.com>
2018-12-03 19:13:18 -0600

    Price and checkout funtionality

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Lettuce', type: 'lettuce' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+    { label: 'Turkey Bacon', type: 'turkeyBacon' }
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        <p>Current Price: <span className={classes.TotalPrice}>${props.price.toFixed(2)}</span></p>
+        {controls.map(ctrl => (
+            <BuildControl 
+                added={() => props.ingredientAdded(ctrl.type)} 
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                key={ctrl.label} 
+                label={ctrl.label} 
+                disabled={props.disabled[ctrl.type]} />
+        ))}
+        <button disabled={!props.purchaseable} className={classes.OrderButton}>CHECKOUT</button>
+    </div>
+);
+
+export default buildControls;
